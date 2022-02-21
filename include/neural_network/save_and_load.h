3e7796c9ced6665a6230f28926209d1c4131d1f8
Louca Guermeur <louca.guermeur@epita.fr>
2022-02-21 01:26:43 +0100

    update load

--- include/neural_network/save_and_load.h
+++ include/neural_network/save_and_load.h
@@ -2,6 +2,9 @@
 #define SAVE_AND_LOAD_H
 
 #include "neural_network.h"
-void saveNetwork(Network network, char name[]);
+
+void saveNetwork(Network* network, char name[]);
+
+Network* loadNetwork(char name[]);
 
 #endif
--- source/neural_network/neural_network.c
+++ source/neural_network/neural_network.c
@@ -6,43 +6,70 @@
 #include <time.h>
 #include "../../include/neural_network/neural_network.h"
 
-Neuron newNeuron(unsigned int nb_weights){
-	Neuron neuron = {
-		.nb_weights = nb_weights,
-		.bias = 0,
-		.activation = 0,
-		.weights = NULL 
-	};
-	neuron.weights = malloc(sizeof(double) * nb_weights);
-	if(neuron.weights == NULL){
-		errx(1, "something goes wrong");
+Neuron* newNeuron(unsigned int nb_weights){
+	// allocating memory for the neuron
+	Neuron* neuron = NULL;
+	neuron = malloc(sizeof(Neuron));
+	if(neuron == NULL){
+		errx(1, "Not enough memory!");
 	}
+
+	// init the neuron values
+	neuron->nb_weights = nb_weights;
+	neuron->bias = 0;
+	neuron->activation = 0;
+	neuron->weights = NULL;
+
+	// allocating memory for the weights
+	neuron->weights = malloc(sizeof(double) * nb_weights);
+	if(neuron->weights == NULL){
+		errx(1, "Not enough memory!");
+	}
+
+	// to init weights you should use initNetwork() or loadNetwork function
+
 	return neuron;
 }
 
 
-Layer newLayer(unsigned int size, unsigned int previous_layer_size){
-	Layer layer = {
-		.nb_neurons = size,
-		.neurons = NULL
-	};
-	
-	layer.neurons = malloc(sizeof(Neuron) * size);
+Layer* newLayer(unsigned int size, unsigned int previous_layer_size){
+	// allocating memory for the layer
+	Layer* layer = NULL;
+	layer = malloc(sizeof(Layer));
+	if(layer == NULL){
+		errx(1, "Not enough memory!");
+	}
 
-	/* create neurons for layer */
+	// init the layer values
+	layer->nb_neurons = size;
+	layer->neurons = NULL;
+	
+	// allocating memory for the neurons
+	layer->neurons = malloc(sizeof(Neuron*) * size);
+	if(layer->neurons == NULL){
+		errx(1, "Not enough memory!");
+	}
+
+	// create neurons for layer
 	for(unsigned int i = 0; i < size; i++){
-		layer.neurons[i] = newNeuron(previous_layer_size);
+		layer->neurons[i] = newNeuron(previous_layer_size);
 	}
 	
 	return layer;
 }
 
-Network newNetwork(unsigned int sizes[], unsigned int nb_layers){
+Network* newNetwork(unsigned int sizes[], unsigned int nb_layers){
+	// allocating memory for the network
+	Network* network = NULL;
+	network = malloc(sizeof(Network));
+	if(network == NULL){
+		errx(1, "Not enough memory!");
+	}
+
 	// init the network values
-	Network network = {
-		.nb_layers = nb_layers,
-		.layers = NULL
-	};
+	network->nb_layers = nb_layers;
+	network->nb_layers = nb_layers;
+	network->layers = NULL;
 	
 	// checking if network has a good format
 	if(nb_layers < 3){
@@ -53,12 +80,17 @@ Network newNetwork(unsigned int sizes[], unsigned int nb_layers){
 			errx(1, "Bad neural network format");
 		}
 	}
+
 	// allocating memory for the layers
-	network.layers = malloc(sizeof(Layer) * nb_layers);
+	network->layers = malloc(sizeof(Layer*) * nb_layers);
+	if(network->layers == NULL){
+		errx(1, "Not enough memory!");
+	}
+
 	// create layers for network
-	network.layers[0] = newLayer(sizes[0], 0);
+	network->layers[0] = newLayer(sizes[0], 0);
 	for(unsigned int i = 1; i < nb_layers; i++){
-		network.layers[i] = newLayer(sizes[i], sizes[i - 1]);
+		network->layers[i] = newLayer(sizes[i], sizes[i - 1]);
 	}
 	
 	return network;
@@ -66,74 +98,79 @@ Network newNetwork(unsigned int sizes[], unsigned int nb_layers){
 
 
 
-void initNetwork(Network network){
+Network* initNetwork(unsigned int sizes[], unsigned int nb_layers){
+	Network* network = newNetwork(sizes, nb_layers);
+
 	srand(time(NULL));
-	for(unsigned int i = 1; i < network.nb_layers; i++){
-		for(unsigned int j = 0; j < network.layers[i].nb_neurons; j++){
+	for(unsigned int i = 1; i < network->nb_layers; i++){
+		for(unsigned int j = 0; j < network->layers[i]->nb_neurons; j++){
 			for(unsigned int l = 0;
-			l < network.layers[i].neurons[j].nb_weights; l++){
-				network.layers[i].neurons[j].weights[l] =
+			l < network->layers[i]->neurons[j]->nb_weights; l++){
+				network->layers[i]->neurons[j]->weights[l] =
 				 rand() % 10;
 			}
-			network.layers[i].neurons[j].bias =
+			network->layers[i]->neurons[j]->bias =
 			 rand() % 10;
 		}
 	}
+
+	return network;
 }
 
 
-void freeNetwork(Network network){
-	for(unsigned int i = 0; i < network.nb_layers; i++){
-		for(unsigned int j = 0; j < network.layers[i].nb_neurons; j++){
-			free(network.layers[i].neurons[j].weights);
+void freeNetwork(Network* network){
+	for(unsigned int i = 0; i < network->nb_layers; i++){
+		for(unsigned int j = 0; j < network->layers[i]->nb_neurons; j++){
+			free(network->layers[i]->neurons[j]->weights);
 		}
-		free(network.layers[i].neurons);
+		free(network->layers[i]->neurons);
 	}
-	free(network.layers);
+	free(network->layers);
+	free(network);
 }
 
 
-void feedforward(Network network, double inputs[]){
+void feedforward(Network* network, double inputs[]){
 	// add the inputs in the neural network
-	unsigned int nb_layers = network.nb_layers;
-	for(unsigned int i = 0; i < network.layers[0].nb_neurons; i++){
-	network.layers[0].neurons[i].activation = inputs[i];
+	unsigned int nb_layers = network->nb_layers;
+	for(unsigned int i = 0; i < network->layers[0]->nb_neurons; i++){
+	network->layers[0]->neurons[i]->activation = inputs[i];
 	}
 	double sum = 0;
 	// proceed to the feedforward for the hiddens layers
 	// the loop don't go into the first layer and the last layer
 	for(unsigned int i = 1; i < nb_layers - 1; i++){
-		for(unsigned int j = 0; j < network.layers[i].nb_neurons; j++){
+		for(unsigned int j = 0; j < network->layers[i]->nb_neurons; j++){
 			for(unsigned int l = 0;
-			l < network.layers[i].neurons[j].nb_weights; l++){
+			l < network->layers[i]->neurons[j]->nb_weights; l++){
 				/* i.e. activation of the neuron in the previous 
 				layer multiply by the weight of the actual neuron */
-				sum += network.layers[i-1].neurons[l].activation *
-				network.layers[i].neurons[j].weights[l];
+				sum += network->layers[i-1]->neurons[l]->activation *
+				network->layers[i]->neurons[j]->weights[l];
 			}
 			// add the bias
-			sum += network.layers[i].neurons[j].bias;
+			sum += network->layers[i]->neurons[j]->bias;
 			// sigmoid function to the value
-			network.layers[i].neurons[j].activation = sigmoid(sum);
+			network->layers[i]->neurons[j]->activation = sigmoid(sum);
 			sum = 0;
 		}
 	}
 	// same as upper but we don't do the sigmoid func
-	for(unsigned int i = 0; i < network.layers[nb_layers - 1].nb_neurons; i++){
+	for(unsigned int i = 0; i < network->layers[nb_layers - 1]->nb_neurons; i++){
 		for(unsigned int j = 0;
-			       j < network.layers[nb_layers - 1].neurons[i].nb_weights;
+			       j < network->layers[nb_layers - 1]->neurons[i]->nb_weights;
 			       j++){
-			sum += network.layers[nb_layers - 2].neurons[j].activation *
-			 network.layers[nb_layers - 1].neurons[i].weights[j];
+			sum += network->layers[nb_layers - 2]->neurons[j]->activation *
+			 network->layers[nb_layers - 1]->neurons[i]->weights[j];
 		}
-		sum += network.layers[nb_layers - 1].neurons[i].bias;
-		network.layers[nb_layers - 1].neurons[i].activation = sum;
+		sum += network->layers[nb_layers - 1]->neurons[i]->bias;
+		network->layers[nb_layers - 1]->neurons[i]->activation = sum;
 		sum = 0;
 	}
-	softmax(network.layers[nb_layers - 1]);
-	for(unsigned int i = 0; i < network.layers[nb_layers - 1].nb_neurons; i++){
+	softmax(network->layers[nb_layers - 1]);
+	for(unsigned int i = 0; i < network->layers[nb_layers - 1]->nb_neurons; i++){
 		printf("the output neuron number %i as a probability of: %f\n", i + 1,
-		network.layers[network.nb_layers - 1].neurons[i].activation);
+		network->layers[network->nb_layers - 1]->neurons[i]->activation);
 	}
 }
 
@@ -145,23 +182,31 @@ double sigmoid_prime(double x){
 	return sigmoid(x) * (1 - sigmoid(x));
 }
 
-void softmax(Layer layer){
-	unsigned int nb_neurons = layer.nb_neurons;
-	double max = layer.neurons[0].activation;
+void softmax(Layer* layer){
+	unsigned int nb_neurons = layer->nb_neurons;
+
+	// try to find the max value of neurons
+	double max = layer->neurons[0]->activation;
 	for(unsigned int i = 1; i < nb_neurons; i++){
-		if(max < layer.neurons[i].activation){
-			max = layer.neurons[i].activation;
+		if(max < layer->neurons[i]->activation){
+			max = layer->neurons[i]->activation;
 		}
 	}
+
+	// substract all neurons value by the max value found
 	for(unsigned int i = 0; i < nb_neurons; i++){
-	layer.neurons[i].activation = layer.neurons[i].activation - max;
+	layer->neurons[i]->activation = layer->neurons[i]->activation - max;
 	}
+
+	// calculate the divisor of the softmax function
 	double divisor = 0;
 	for(unsigned int i = 0; i < nb_neurons; i++){
-		divisor += (exp(layer.neurons[i].activation));
+		divisor += (exp(layer->neurons[i]->activation));
 	}
+
+	// calculate the value for each neurons
 	for(unsigned int i = 0; i < nb_neurons; i++){
-		layer.neurons[i].activation = 
-			(exp(layer.neurons[i].activation)) / divisor;
+		layer->neurons[i]->activation = 
+			(exp(layer->neurons[i]->activation)) / divisor;
 	}
 }
