dc2a4a2140dc7b820fe372b612a102ff0c28502d
Guillaume PELSSER BANALES <guillaume.pelsser-banales@epita.fr>
2022-03-17 21:56:11 +0100

    Guillaume

--- source/imagery_treatment/pretreatment_color.h
+++ source/imagery_treatment/pretreatment_color.h
@@ -4,4 +4,10 @@ void invert(SDL_Surface *image);
 void contrast(SDL_Surface *image_surface, int fact);
 float color_max(SDL_Surface *image);
 void brightness(SDL_Surface *image);
+int median(int *array);
+int ave(int*array);
+void median_filter(SDL_Surface *image);
+void aver_filter(SDL_Surface *image);
+int otsu_tresholding(SDL_Surface *image);
+void binarize(SDL_Surface *image);
 #endif
